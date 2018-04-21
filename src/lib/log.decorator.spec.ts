6bcb6f01f965f0f3fd9546184c0405e00dd284c4
Davide Cavaliere <cavaliere.davide@gmail.com>
2018-04-21 15:08:49 +0000

    test: adding some tests

--- src/lib/log.decorator.spec.ts
+++ src/lib/log.decorator.spec.ts
@@ -1,28 +1,56 @@
-// tslint:disable:no-expression-statement
+// tslint:disable:no-expression-statement, max-classes-per-file
 import { test } from 'ava';
 import { Log, setNamespace } from './log.decorator';
+import { IDebugger } from 'debug';
+import 'reflect-metadata';
+
 
 setNamespace('myNamespace');
 
 class TestClass {
 
-    @Log()
-    public $l;
+  @Log()
+  private $l;
 
-    constructor() {
-        this.$l('constructing', this);
-    }
+  constructor() {
+    this.$l('constructing', this);
+  }
 }
-
 let instance: TestClass;
 
+setNamespace('ns2');
+
+class TestClassNS2 {
+  @Log()
+  private $l: IDebugger;
+
+  constructor() {
+    this.$l('test 2nd ns');
+  }
+}
+
+let instance2: TestClassNS2;
+
+setNamespace('ns3');
+
+class TestClassNS3 {
+  @Log()
+  private $l: IDebugger;
+
+  constructor() {
+    this.$l('test 3nd ns');
+  }
+}
+
+let instance3: TestClassNS3;
+
 test.beforeEach(() => {
-    instance = new TestClass();
+  instance = new TestClass();
+  instance2 = new TestClassNS2();
+  instance3 = new TestClassNS3();
 });
 
 test('log decorator', t => {
+  t.is(true, true);
 
-    instance.$l('testing log', t);
-
-    t.is(typeof instance.$l, 'function');
 });
