e88207b8c00f3aa2f391e18827f3605b3c7d4ddf
Davide Cavaliere <cavaliere.davide@gmail.com>
2018-04-21 15:08:37 +0000

    refactor: cleaning code

--- src/lib/log.decorator.ts
+++ src/lib/log.decorator.ts
@@ -1,19 +1,24 @@
-import debug from 'debug';
+import debug, { IDebugger } from 'debug';
+import 'reflect-metadata';
 
-const logger = debug;
+export const debuggerMetadata = Symbol('debug');
 let namespace = 'ts-debug';
 
 export function Log(subspace?: string) {
-    return (target, propertyKey) => {
-        subspace = subspace || target.constructor.name;
 
-        target[propertyKey] = (...args: any[]) => {
-            logger([namespace, subspace].join(':')).apply(target, args);
-        }
-    }
+  return (target, propertyKey) => {
+    subspace = subspace || target.constructor.name;
+
+    const loggerFn: IDebugger = debug([namespace, subspace].join(':'));
+    loggerFn['useColors'] = true;
+
+    Reflect.defineMetadata(debuggerMetadata, loggerFn, target);
+
+    target[propertyKey] = loggerFn;
+  }
 }
 
 
 export function setNamespace(ns: string) {
-    namespace = ns;
+  namespace = ns;
 }
