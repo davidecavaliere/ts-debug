c90e73e6771ad86cf29a153be3bf2e79ecfef5bd
Davide Cavaliere <cavaliere.davide@gmail.com>
2018-04-29 13:39:19 +0000

    fix: setting default namespace to null, decorator should set an object on the annotated property. because being a property should not be assigned a fuction to it

--- src/lib/log.decorator.ts
+++ src/lib/log.decorator.ts
@@ -2,19 +2,24 @@ import debug, { IDebugger } from 'debug';
 import 'reflect-metadata';
 
 export const debuggerMetadata = Symbol('debug');
-let namespace = 'ts-debug';
+let namespace;
 
 export function Log(subspace?: string) {
 
   return (target, propertyKey) => {
     subspace = subspace || target.constructor.name;
 
-    const loggerFn: IDebugger = debug([namespace, subspace].join(':'));
+    const ns = namespace ? [namespace, subspace].join(':') : subspace;
+
+    const loggerFn: IDebugger = debug(ns);
     loggerFn['useColors'] = true;
 
     Reflect.defineMetadata(debuggerMetadata, loggerFn, target);
 
-    target[propertyKey] = loggerFn;
+
+    target[propertyKey] = {
+      d: loggerFn
+    };
   }
 }
 
